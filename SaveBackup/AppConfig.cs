8524a911c1ca01b90c9e997b0263c734d2229b02
Mystiick <Mystiick@outlook.com>
2022-04-12 20:07:28 -0500

    Rework backup tool to recursively copy/remove files and folders to fully sync the source and destination folders. Rework process management to no longer require passing around Process and ProcessStartInfo. Add StandardError to RunCommand(). Update InitalizeRepo to properly handle new/removed files

--- SaveBackup/AppConfig.cs
+++ SaveBackup/AppConfig.cs
@@ -1,6 +1,8 @@
 public class AppConfig
 {
-    public string SaveLocation {get;set;} = string.Empty;
-    public string BackupLocation {get;set;} = string.Empty;
-    public bool UseGit {get;set;} = false;
+    public string SaveLocation { get; set; } = string.Empty;
+    public string BackupLocation { get; set; } = string.Empty;
+    public bool UseGit { get; set; } = false;
+    public bool Verbose { get; set; } = false;
+    public bool PauseOnFinish { get; set; } = false;
 }
