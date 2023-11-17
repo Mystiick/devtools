ecdbf47b50548cdfccda8366b22e4f19a192298e
Mystiick <Mystiick@outlook.com>
2023-11-16 22:26:07 -0500

    Update and fix

--- better-twitch-theatre.js
+++ better-twitch-theatre.js
@@ -7,23 +7,23 @@ javascript: (() => {
         catch { console.log(`${className} not found`); }
     }
 
-    var class_beside = "eVTxSo"; /* class twitch assigns the chatbox for the right side */
-    var class_below = "hnyVuh"; /* class for the chat below */
+    const class_beside = "eVTxSo"; /* class twitch assigns the chatbox for the right side */
+    const class_below = "hnyVuh"; /* class for the chat below */
 
     /* Open theatre mode */
-    var t = document.querySelector('[aria-label="Theatre Mode (alt+t)"]');
+    const t = document.querySelector('[aria-label="Theatre Mode (alt+t)"]');
     if (t) t.click();
 
     /* Change chatbox to below the stream */
-    var chat = document.getElementsByClassName("right-column--theatre")[0];
+    const chat = document.getElementsByClassName("right-column--theatre")[0];
     document.getElementsByClassName("persistent-player--theatre")[0].style = "top: 0px; left: 0px; position: fixed; z-index: 3000; height: 56.2vw; transform: scale(1); width: 100vw;";
     chat.className = chat.className.replace("right-column--beside", "right-column--below").replace(class_beside, class_below);
 
     /* Filter through the stylesheets, find the core sheet and add position styles */
-    var c = Array.prototype.filter.call(
+    const c = Array.prototype.filter.call(
         Array.prototype.filter.call(
             document.styleSheets,
-            x => x.href && x.href.indexOf("assets/core") != -1 /*"url like https://static.twitchcdn.net/assets/core-57b1d4b350873f588661.css"*/
+            x => x.href && x.href.indexOf("assets/core") !== -1 /*"url like https://static.twitchcdn.net/assets/core-57b1d4b350873f588661.css"*/
         )[0].cssRules,
         y => y.selectorText && y.selectorText.indexOf("right-column--below") != -1
     )[0];
@@ -37,4 +37,14 @@ javascript: (() => {
     hide("right-column__toggle-visibility", e => e[0]); /* expand arrow */
     hide("paid-pinned-chat-message-list", e => e[0]); /* paid chats */
     hide("Layout-sc-1xcs6mc-0 ivcMfs", e => e[0].parentElement.parentElement); /* more paid chats */
+
+    /* Move chatbox in between  */
+    const chatbox = document.getElementsByClassName("chat-input__textarea")[0].parentNode;
+    const chatbar = document.getElementsByClassName("chat-input__buttons-container")[0];
+    const chatbuttons = document.getElementsByClassName("Layout-sc-1xcs6mc-0 kEPLoI")[0]; /* Send chat/options button container */
+    chatbar.appendChild(chatbox);
+    chatbar.appendChild(chatbuttons);
+    chatbar.style = "margin-top:0 !important";
+
+    chatbox.style = "width:100%";
 })();
