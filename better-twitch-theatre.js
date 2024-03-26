1cade24f15dcd7e6a91f25f552338083ec474468
Mystiick <mystiick@outlook.com>
2024-03-25 20:24:30 -0400

    Add timeout

--- better-twitch-theatre.js
+++ better-twitch-theatre.js
@@ -14,27 +14,29 @@ javascript: (() => {
     var t = document.querySelector('[aria-label="Theatre Mode (alt+t)"]');
     if (t) t.click();
 
-    /* Change chatbox to below the stream */
-    var chat = document.getElementsByClassName("right-column--theatre")[0];
-    document.getElementsByClassName("persistent-player--theatre")[0].style = "top: 0px; left: 0px; position: fixed; z-index: 3000; height: 56.2vw; transform: scale(1); width: 100vw;";
-    chat.className = chat.className.replace("right-column--beside", "right-column--below").replace(class_beside, class_below);
+    setTimeout(() => {
+        /* Change chatbox to below the stream */
+        var chat = document.getElementsByClassName("right-column--theatre")[0];
+        document.getElementsByClassName("persistent-player--theatre")[0].style = "top: 0px; left: 0px; position: fixed; z-index: 3000; height: 56.2vw; transform: scale(1); width: 100vw;";
+        chat.className = chat.className.replace("right-column--beside", "right-column--below").replace(class_beside, class_below);
 
-    /* Filter through the stylesheets, find the core sheet and add position styles */
-    var c = Array.prototype.filter.call(
-        Array.prototype.filter.call(
-            document.styleSheets,
-            x => x.href && x.href.indexOf("assets/core") != -1 /*"url like https://static.twitchcdn.net/assets/core-57b1d4b350873f588661.css"*/
-        )[0].cssRules,
-        y => y.selectorText && y.selectorText.indexOf("right-column--below") != -1
-    )[0];
-    c.style["position"] = "absolute";
-    c.style["left"] = "0";
-    c.style["bottom"] = "0";
+        /* Filter through the stylesheets, find the core sheet and add position styles */
+        var c = Array.prototype.filter.call(
+            Array.prototype.filter.call(
+                document.styleSheets,
+                x => x.href && x.href.indexOf("assets/core") != -1 /*"url like https://static.twitchcdn.net/assets/core-57b1d4b350873f588661.css"*/
+            )[0].cssRules,
+            y => y.selectorText && y.selectorText.indexOf("right-column--below") != -1
+        )[0];
+        c.style["position"] = "absolute";
+        c.style["left"] = "0";
+        c.style["bottom"] = "0";
 
-    /*  Hide individual elements */
-    hide("Layout-sc-1xcs6mc-0 DGdsv", e => e[0].parentNode);  /* channel leaderboard */
-    hide("stream-chat-header", e => e[0]); /* 'stream chat' header */
-    hide("right-column__toggle-visibility", e => e[0]); /* expand arrow */
-    hide("paid-pinned-chat-message-list", e => e[0]); /* paid chats */
-    hide("Layout-sc-1xcs6mc-0 ivcMfs", e => e[0].parentElement.parentElement); /* more paid chats */
+        /*  Hide individual elements */
+        hide("Layout-sc-1xcs6mc-0 DGdsv", e => e[0].parentNode);  /* channel leaderboard */
+        hide("stream-chat-header", e => e[0]); /* 'stream chat' header */
+        hide("right-column__toggle-visibility", e => e[0]); /* expand arrow */
+        hide("paid-pinned-chat-message-list", e => e[0]); /* paid chats */
+        hide("Layout-sc-1xcs6mc-0 ivcMfs", e => e[0].parentElement.parentElement); /* more paid chats */
+    });
 })();
